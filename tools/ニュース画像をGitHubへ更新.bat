5b27997ad267872d064b3e8e1836230b7e13896c
Ampere coulomb <111sou26go@gmail.com>
2026-08-21 02:51:47 +0900

    Update site files 2026-08-21 02:51

--- tools/ニュース画像をGitHubへ更新.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0update_news_images_to_github.ps1"
-pause
-
--- tools/画像を720x405のwebpに圧縮.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0resize-webp-720x405.ps1" %*
-pause
-
